d27372ca7a3b0688692a50f427a4235e52e88212
hiryus <hiryus.sha@gmail.com>
2018-02-06 22:10:12 +0000

    Added a script generating serialized room data.

---
+++ utils/roomSerializer.js
@@ -0,0 +1,10 @@
+(function(roomName) {
+    let str = '';
+    for (let x = 0; x < 50; x += 1) {
+        for (let y = 0; y < 50; y += 1) {
+            const terrain = Game.map.getTerrainAt(x, y, roomName);
+            str += ['plain', 'wall', 'swamp'].indexOf(terrain);
+        }
+    }
+    return str;
+})('W15S1');
