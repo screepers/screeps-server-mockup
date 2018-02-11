4dc225de96b06fe68e375cff9205fc7708111304
hiryus <hiryus.sha@gmail.com>
2018-02-11 12:51:02 +0000

    Updated serializer utility to wall some exits.

--- utils/roomSerializer.js
+++ utils/roomSerializer.js
@@ -1,10 +1,15 @@
 (function(roomName) {
     let str = '';
+    const TYPES = ['plain', 'wall', 'swamp'];
     for (let y = 0; y < 50; y += 1) {
         for (let x = 0; x < 50; x += 1) {
-            const terrain = Game.map.getTerrainAt(x, y, roomName);
-            str += ['plain', 'wall', 'swamp'].indexOf(terrain);
+            if (x === 0) { // <-------------------------------------| adjust condition to your needs
+                str += TYPES.indexOf('wall'); //                    | in order to wall some exists
+            } else {
+                const terrain = Game.map.getTerrainAt(x, y, roomName);
+                str += TYPES.indexOf(terrain);
+            }
         }
     }
     return str;
-})('W15S1');
+})('W15S1'); // <---------------------------------------------------| adjust room name to your needs
